52faed53930bfe37c5c172a2bf6912dc57031889
spainisnotequal <spainisnotequal@gmail.com>
2020-04-05 19:26:34 +0200

    Connect to the database

--- cl-web-server.asd
+++ cl-web-server.asd
@@ -7,6 +7,7 @@
   :version "0.0.1"
   :serial t
   :depends-on (#:hunchentoot
-               #:uiop)
+               #:uiop
+               #:postmodern)
   :components ((:file "package")
                (:file "server")))
--- server.lisp
+++ server.lisp
@@ -2,6 +2,10 @@
 
 (in-package #:cl-web-server)
 
+;;; ----------------
+;;; Start the server
+;;; ----------------
+
 (defvar *acceptor* nil)
 
 (defun start-server (&optional (port 5000))
@@ -11,3 +15,33 @@
         (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor
                                           :port port
                                           :document-root (uiop:parse-unix-namestring "./www/")))))
+
+(start-server)
+
+;;; -----------------------
+;;; Connect to the database
+;;; -----------------------
+
+;; define connection parameters
+
+(defvar *database-url* nil) ; If the database is in Heroku, use: (defvar *database-url* (heroku-getenv "DATABASE_URL"))
+(defvar *local-db-params* (list "mydb" "lispuser" "lisppassword" "localhost"))
+
+(defun db-params ()
+  "Heroku database url format is postgres://username:password@host/database_name. If we are testing on localhost, use the db-parameters from *local-db-params*."
+  (if *database-url*
+      (let* ((url (second (cl-ppcre:split "//" *database-url*)))
+	     (user (first (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
+	     (password (second (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
+	     (host (first (cl-ppcre:split "/" (second (cl-ppcre:split "@" url)))))
+	     (database (second (cl-ppcre:split "/" (second (cl-ppcre:split "@" url))))))
+	(list database user password host))
+      *local-db-params*))
+
+;; test the connection
+(postmodern:with-connection (db-params)
+  (postmodern:query (:select (:+ 1 2)) :single))
+
+;; get all data from the "gold_price" table
+(postmodern:with-connection (db-params)
+  (postmodern:query (:select '* :from 'gold_price)))
